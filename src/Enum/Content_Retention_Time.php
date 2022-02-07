332c0c5b25573f7296dd6e8844c8406aac0152e8
Gabriel Trosell <gabriel.trosell@gmail.com>
2022-02-07 20:16:20 +0100

    clarified the value unit

--- src/Enum/Content_Retention_Time.php
+++ src/Enum/Content_Retention_Time.php
@@ -5,6 +5,13 @@ namespace DeployHuman\kivra\Enum;
 
 enum Content_Retention_Time: string
 {
+    /**
+     * The content will be deleted after the specified time, in days
+     */
     case time_30 = '30';
+
+    /**
+     * The content will be deleted after the specified time, in days
+     */
     case time_390 = '390';
 }
